22965416efd44dd42c0ee9d4b60b1a160f53fd61
Vitalii Chornous <v.chornous@apiko.com>
2021-06-02 13:57:17 +0300

    added missing email templates

--- src/controllers/auth-controller.js
+++ src/controllers/auth-controller.js
@@ -1,7 +1,7 @@
 const createError = require('http-errors');
 
 const User = require('../models/user');
-const catchAsync = require('../utils/catchAsync');
+const catchAsync = require('../utils/catch-async');
 const httpCodes = require('../constants/http-codes');
 const responseStatus = require('../constants/response-status');
 const { createSendToken } = require('../utils/auth');
